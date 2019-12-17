707467184999d38daebd64d48d017bccb1c606f2
Anan jaser <ananjaser@gmail.com>
2019-12-17 23:55:53 +0400

    drivers: soc: by-pass GPU voltage restrictions
    * Values aquired from stock j5y17lte reading
    * set missing 1.2/1.3x voltages

--- drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_custom_interface.c
+++ drivers/gpu/arm/t83x/r28p0/platform/exynos/gpu_custom_interface.c
@@ -58,6 +58,14 @@
 #include <mach/apm-exynos.h>
 #endif
 
+#ifdef CONFIG_SOC_EXYNOS7870
+#define GPU_MAX_VOLT		1300000
+#define GPU_MIN_VOLT		500000
+#define GPU_VOLT_STEP		6250
+#else
+#error "Please define gpu voltage ranges for current SoC."
+#endif
+
 extern struct kbase_device *pkbdev;
 
 int gpu_pmqos_dvfs_min_lock(int level)
